3436838a13a08fdeadb4dafbdd145a033485a65e
Alice Sn0w <snow.alice@pm.me>
2023-08-03 10:48:49 +0200

    add examples

---
+++ examples/knowledge-inject.js
@@ -0,0 +1,16 @@
+const thehive4nodejs = require('./index.js');
+
+
+// Change this
+const myThehive = new thehive4nodejs(
+    "http://127.0.0.1", "9000", ""
+)
+
+/**
+ * Delete old knowledge database and inject new one
+ */
+async function main() {
+    await myThehive.custom.knowledge.injectKnowledgeDatabase("/path/to/my/database") // change the path
+}
+
+main()
